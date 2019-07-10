ad680a4d656466b6d0901821ab111b22266bd5f2
Jørgen <jorgen@baezeni.com>
2019-07-10 16:10:05 +0700

    adding auth

---
+++ exampleAPI/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exampleAPI.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            var email = collection["email"];
+            var password = collection["password"];
+
+            Session["email"] = email;
+
+            return Content(Session["email"].ToString());
+        }
+       
+
+        // GET: Auth/Create
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Auth/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auth/Edit/5
+      
+    }
+}
